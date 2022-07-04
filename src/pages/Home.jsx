73f8ffbf199ac1a8cc1a61b6e92794ac8ba8aafc
KImSuim <suim7171@naver.com>
2022-07-05 00:42:54 +0900

    home 스타일 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,20 @@
 import Navigation from "../components/Navigation";
 import ThemeButton from "../components/ThemeButton";
 import ProductCard from "../components/ProductCard";
-
+import styled from "styled-components";
 const Home = () => {
     return (
         <div>
             <div>
                 <Navigation/>
-                {/*ThemeButton */}
-                <ThemeButton themeName={"#겨울방한템"}/>
-                <ThemeButton themeName={"#따순머그컵"}/>
+                <ThemeSection>
+                    <ThemeButton themeName={"#겨울방한템"}/>
+                    <ThemeButton themeName={"#따순머그컵"}/>
+                </ThemeSection>
+
+                <GrayLine/>
             </div>
-            <div>
+            <ProductSection>
                 {/* ProductCard*/}
                 <ProductCard
                     name="비숑 블랙 머그잔"
@@ -28,11 +31,25 @@ const Home = () => {
                     description="솜사탕처럼 부드러운 쉐입에 스트라이프 조각이 더해져 심플하면서도 세련된 파스텔 컬러의 머그"
                     thumbnail="https://raw.githubusercontent.com/congchu/coment-shop-server/master/assets/images/product3.jpg"
                 />
-            </div>
-            
-            
-
+            </ProductSection>
         </div>
     );
 };
+
+const ThemeSection = styled.div`
+    display:flex;
+    gap:12px;
+    padding: 40px 12px;
+`;
+
+const GrayLine = styled.div`
+    height: 8px;
+    width: 10px;
+    background: #eeeeee;
+`;
+
+const ProductSection = styled.div`
+    padding: 20px;
+`;
+
 export default Home;
