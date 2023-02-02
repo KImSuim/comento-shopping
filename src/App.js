9e5cb6a3821ff7283a82332b368e5671b963f95e
KImSuim <suim7171@naver.com>
2023-02-02 13:37:04 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from "./pages/Home"
-import ProductDetail from "./pages/ProductDetail"
-import Basket from "./pages/Basket"
+import Home from './pages/Home';
+import ProductDetail from './pages/ProductDetail';
+import Basket from './pages/Basket';
 
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter basename='comento-shopping'>
+    <BrowserRouter basename="comento-shopping">
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/product/:productId" element={<ProductDetail />}/>
-        <Route path="/Basket" element={<Basket />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:productId" element={<ProductDetail />} />
+        <Route path="/Basket" element={<Basket />} />
       </Routes>
     </BrowserRouter>
   );
